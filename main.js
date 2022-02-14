be187250112c1268abd1976561d06fbccac9963a
major davis <majordav@msn.com>
2022-02-14 12:04:36 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function change(){
+    onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
+}
+
+function goBack(){
+    onmouseout = document.getElementById('whatIf').innerHTML='this will change';
+}
+
