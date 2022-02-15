1c71f037cf4ed8523ba1e87d214c292b813182ab
major davis <majordav@msn.com>
2022-02-14 17:02:14 -0700

    created html, js, css files for to do list

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ function goBack(){
     onmouseout = document.getElementById('whatIf').innerHTML='this will change';
 }
 
+function Third(){
+    onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
+
+}
+
+function thisIsIt() {
+    onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
+}
+
