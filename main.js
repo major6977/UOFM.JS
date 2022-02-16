f9c92a63cdb3144612366314816bc0ae9aaa6a8d
major davis <majordav@msn.com>
2022-02-15 20:11:01 -0700

    added functions js, added color in css, added p tags in html

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 
-function change(){
+ function change(){
     onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
-}
+ }
 
-function goBack(){
-    onmouseout = document.getElementById('whatIf').innerHTML='this will change';
-}
+ function goBack(){
+     onmouseout = document.getElementById('whatIf').innerHTML='this will change';
+ }
 
-function Third(){
+ function Third(){
     onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
 
+ }
+
+ function thisIsIt() {
+     onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
+ }
+
+
+function closeMe() {
+    // find the element
+    x=document.getElementById('demo');
+    //Option 1: Change the style attribute directly
+    x.style.display='none';
+
+    //Option 2 Change the class
+    // x.className ='close;
 }
 
-function thisIsIt() {
-    onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
-}
+    function openMe() {
+       
 
+    function color() {
+            document.getElementById('demo').style.color='red';   
+        }
+       
+        // 2: Change the class
+        //x.className='open';
+
+         //find the element
+         x=document.getElementById('demo');
+         //Option 1: Change the style attribute directly
+         x.style.display='block';
+    }
+    
