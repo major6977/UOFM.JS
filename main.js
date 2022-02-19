8ea0c01eb23c4b2798d47b3c7a1dcb74513c6e9d
major davis <majordav@msn.com>
2022-02-18 19:29:38 -0700

    added another button color green

--- main.js
+++ main.js
@@ -1,41 +1,69 @@
 
- function change(){
-    onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
- }
+//  function change(){
+//     onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
+//  }
 
- function goBack(){
-     onmouseout = document.getElementById('whatIf').innerHTML='this will change';
- }
+//  function goBack(){
+//      onmouseout = document.getElementById('whatIf').innerHTML='this will change';
+//  }
 
- function Third(){
-    onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
+//  function Third(){
+//     onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
 
- }
+//  }
 
- function thisIsIt() {
-     onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
- }
+//  function thisIsIt() {
+//      onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
+//  }
 
 
-function closeMe() {
-    // find the element
-    x=document.getElementById('demo');
-    //Option 1: Change the style attribute directly
-    x.style.display='none';
+// function closeMe() {
+//     // find the element
+//     x=document.getElementById('demo');
+//     //Option 1: Change the style attribute directly
+//     x.style.display='none';
 
-    //Option 2 Change the class
-    // x.className ='close;
-}
+//     //Option 2 Change the class
+//     // x.className ='close;
+// }
 
-    function openMe() {
+//     function openMe() {
        
 
-        // 2: Change the class
-        //x.className='open';
+//         // 2: Change the class
+//         //x.className='open';
 
-         //find the element
-         x=document.getElementById('demo');
-         //Option 1: Change the style attribute directly
-         x.style.display='block';
+//          //find the element
+//          x=document.getElementById('demo');
+//          //Option 1: Change the style attribute directly
+//          x.style.display='block';
+//     }
+
+    // NEW LINE OF CODE
+
+    let fruits = ["Banana", "Orange", "Mango", "Apple"];
+
+    function loadFruits(){
+        document.getElementById("fruits").innerHTML = fruits;
+    }
+
+    function myFunction(){
+        let fruit = prompt("What is Your Favorite Fruit?");
+        fruits[fruits.length] = fruit;
+        document.getElementById("fruits").innerHTML = fruits;
+    }
+
+
+
+    let cars = ["Honda", "Toyota", "Nissan"];
+
+    function loadCars(){
+        document.getElementById("cars").innerHTML = cars;
+    }
+
+    function theFunction(){
+        let cars = prompt("Name Your Favorite Model Car!");
+        cars[cars.length] = cars;
+        document.getElementById("cars").innerHTML = cars;
     }
     
