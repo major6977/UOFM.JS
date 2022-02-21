3d5de71ef8c3e227efc249742fbf868b0a7c8a54
major davis <majordav@msn.com>
2022-02-20 18:42:04 -0700

    added loop function

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
-  function change(){
-     onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
-  }
+//   function change(){
+//      onmouseover = document.getElementById('whatIf').innerHTML='Help ME Out';
+//   }
 
-  function goBack(){
-      onmouseout = document.getElementById('whatIf').innerHTML='this will change';
-  }
+//   function goBack(){
+//       onmouseout = document.getElementById('whatIf').innerHTML='this will change';
+//   }
 
-  function Third(){
-     onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
+//   function Third(){
+//      onmouseout = document.getElementById('hitMe').innerHTML='this will change as well';
 
-  }
+//   }
 
-  function thisIsIt() {
-      onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
-  }
+//   function thisIsIt() {
+//       onmouseover = document.getElementById('hitMe').innerHTML='give it to me';
+//   }
 
 
 // function closeMe() {
@@ -44,27 +44,38 @@
         
  
    
-    let fruits = ["Banana", "Orange", "Mango", "Apple"];
-        function loadFruits(){ 
-        document.getElementById("fruits").innerHTML = fruits; 
-        loadCars();
-}
+//     let fruits = ["Banana", "Orange", "Mango", "Apple"];
+//         function loadFruits(){ 
+//         document.getElementById("fruits").innerHTML = fruits; 
+//         loadCars();
+// }
 
-let cars = ["Honda", "Toyota", "Nissan"];
-function loadCars(){ 
-document.getElementById("cars").innerHTML = cars;
-}
+// let cars = ["Honda", "Toyota", "Nissan"];
+// function loadCars(){ 
+// document.getElementById("cars").innerHTML = cars;
+// }
 
-function myFunction(){
-    let fruit = prompt("What is Your Favorite Fruit?");
-    fruits[fruits.length] = fruit;
-    document.getElementById("fruits").innerHTML = fruits;
-}
-    function theFunction(){
-        let pop = prompt("Name Your Favorite Model Car!");
-    cars[cars.length] = pop;
-        document.getElementById("cars").innerHTML = cars;
+// function myFunction(){
+//     let fruit = prompt("What is Your Favorite Fruit?");
+//     fruits[fruits.length] = fruit;
+//     document.getElementById("fruits").innerHTML = fruits;
+// }
+//     function theFunction(){
+//         let pop = prompt("Name Your Favorite Model Car!");
+//     cars[cars.length] = pop;
+//         document.getElementById("cars").innerHTML = cars;
+//     } 
+
+    
+    let sum = [80, 87, 94, 82, 62, 98, 81, 81, 74, 91];
+    for (index = 0; index < grades.length; index++){ 
+        sum += grades[index];
     }
+    document.write(sum/grades.length);
+
+    
+
+    
 
 
     
